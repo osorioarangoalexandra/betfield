d440c16c46ec62bd33c2d88c4946c6a7298710f6
“Alexandra <osorioarangoalexandra@gmail.com>
2020-02-22 18:51:16 -0500

    Updated Views

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   end
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: "schedules#index"
+
 end
 
